a2c1f9476f190f7821829707be5481c15e97f77b
Nisal Amarasekara <nisalamarasekara@gmail.com>
2026-07-23 16:13:38 +0530

    change

--- angular-app/src/app/common-component/footer/footer.component.ts
+++ angular-app/src/app/common-component/footer/footer.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core'
 import { LucideAngularModule } from 'lucide-angular'
-import { PRIMARY } from '../../core/service/traceability/traceability-data.service'
 
-const TAGS = ['Digital Product Passport', 'Blockchain Verified', 'GS1 Compatible', 'ISO 22095']
+const TAGS = ['Digital Product Passport', 'GS1 Compatible', 'ISO 22095']
 
 @Component({
   selector: 'app-footer',
@@ -13,5 +12,4 @@ const TAGS = ['Digital Product Passport', 'Blockchain Verified', 'GS1 Compatible
 })
 export class FooterComponent {
   tags = TAGS
-  readonly PRIMARY = PRIMARY
 }
